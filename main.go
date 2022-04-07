b6ed8800429695af249609f3b1874cc7d42ce6c7
esllo <master@esllo.com>
2022-04-07 09:31:54 +0900

    update createObject func

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import "syscall/js"
 func main() {
 	channel := make(chan interface{}, 0)
 	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("createObject", js.FuncOf(createObject))
 	<-channel
 }
 
 func add(this js.Value, args []js.Value) interface{} {
 	return args[0].Int() + args[1].Int()
+}
+
+func createObject(this js.Value, args []js.Value) interface{} {
+	return map[string]interface{}{
+		"name": "Hong Gildong",
+		"age":  26,
+	}
 }
