333b1f5c2019ddff9d19248591de9fe81c599441
esllo <master@esllo.com>
2022-04-07 09:25:13 +0900

    update add func

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "syscall/js"
 
+func main() {
+	channel := make(chan interface{}, 0)
+	js.Global().Set("add", js.FuncOf(add))
+	<-channel
+}
+
+func add(this js.Value, args []js.Value) interface{} {
+	return args[0].Int() + args[1].Int()
 }
